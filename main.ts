8ab5629984e4d9a370f81c8cd215e1283cede11e
mcalaviaf <mcalaviaf@icloud.com>
2025-04-03 18:28:10 +0200

    modificados valores de cuenta atras de 3 a 10

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-for (let index = 0; index <= 2; index++) {
-    basic.showNumber(3 - index)
+for (let index = 0; index <= 9; index++) {
+    basic.showNumber(10 - index)
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 }
 music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
